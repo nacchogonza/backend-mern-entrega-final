d212079969e057c93a5dbc75fc4a90b90de3e49a
nacchogonza <nachomgonzalez93@gmail.com>
2021-06-14 23:41:21 -0300

    add normalizr lib & normalization json

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -1,127 +1,200 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import normalizr from 'normalizr';
+const {schema} = normalizr
+const normalize = normalizr.normalize;
+import util from 'util'
+
+function print(objeto) {
+  console.log(util.inspect(objeto, false, 12, true))
+}
+
+const schemaAuthor = new schema.Entity('author', {}, { idAttribute: 'email'})
+
+const messages = new schema.Entity('messages', {
+  author: schemaAuthor,
+}, {idAttribute: '_id'})
+
 
 /* SCHEMAS */
 
 const mensajesSchema = new mongoose.Schema({
   author: {
+    email: {
       type: String,
       require: true,
-      max: 100
+      max: 100,
+    },
+    nombre: {
+      type: String,
+      require: true,
+      max: 100,
+    },
+    apellido: {
+      type: String,
+      require: true,
+      max: 100,
+    },
+    edad: {
+      type: String,
+      require: true,
+      max: 100,
+    },
+    alias: {
+      type: String,
+      require: true,
+      max: 100,
+    },
+    avatar: {
+      type: String,
+      require: true,
+      max: 100,
+    },
   },
   text: {
-      type: String,
-      require: true,
-      max: 255
+    type: String,
+    require: true,
+    max: 255,
   },
   date: {
     type: String,
     require: true,
-    max: 100
+    max: 100,
   },
 });
 
 const productosSchema = new mongoose.Schema({
   title: {
-      type: String,
-      require: true,
-      max: 100
+    type: String,
+    require: true,
+    max: 100,
   },
   price: {
-      type: Number,
-      require: true,
+    type: Number,
+    require: true,
   },
   thumbnail: {
     type: String,
     require: true,
-    max: 255
+    max: 255,
   },
 });
 
 /* MODELS */
 
-const DaoMensajes = mongoose.model('mensajes', mensajesSchema);
-const DaoProductos = mongoose.model('productos', productosSchema);
+const DaoMensajes = mongoose.model("mensajes", mensajesSchema);
+const DaoProductos = mongoose.model("productos", productosSchema);
 
 /* DB CONNECT */
 
-const URL = 'mongodb+srv://root:root@cluster0.j4zse.mongodb.net/ecommerce2?retryWrites=true&w=majority';
+const URL =
+  "mongodb+srv://root:root@cluster0.j4zse.mongodb.net/ecommerce2?retryWrites=true&w=majority";
 
 export const connectDB = () => {
   try {
     mongoose.connect(URL, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
-    console.log('Base de datos conectada!')
+    console.log("Base de datos conectada!");
   } catch (error) {
-    console.log('Error al conectar a DB: ', error); 
+    console.log("Error al conectar a DB: ", error);
   }
-}
+};
 
 /* MESSAGES FUNCTIONS */
 
-export const findMessages = async  () => {
+export const findMessages = async () => {
   try {
     const data = await DaoMensajes.find({});
-    return data
+    const dataJson = JSON.parse(JSON.stringify(data))
+    /* normalizador */
+    const normalizedData = normalize(dataJson, [messages])
+    print(normalizedData)
+
+
+    return normalizedData;
   } catch (error) {
-    console.log('Error al obtener mensajes: ', error); 
+    console.log("Error al obtener mensajes: ", error);
   }
-}
+};
 
 export const insertMessage = async (newMessage) => {
   try {
-    return await DaoMensajes.create({author: newMessage.author, date: newMessage.date, text: newMessage.text});
+    return await DaoMensajes.create({
+      author: {
+        email: newMessage.email,
+        nombre: newMessage.nombre,
+        apellido: newMessage.apellido,
+        edad: newMessage.edad,
+        alias: newMessage.alias,
+        avatar: newMessage.avatar,
+      },
+      text: newMessage.text,
+      date: newMessage.date,
+    });
   } catch (error) {
-    console.log('Error al insertar mensaje: ', error); 
+    console.log("Error al insertar mensaje: ", error);
   }
-}
+};
 
 /* PRODUCTS FUNCTIONS */
 
-export const findProducts = async  () => {
+export const findProducts = async () => {
   try {
-    const products =  await DaoProductos.find({})
+    const products = await DaoProductos.find({});
     return products;
   } catch (error) {
-    console.log('Error al obtener productos: ', error); 
+    console.log("Error al obtener productos: ", error);
   }
-}
-export const findProduct = async  (id) => {
+};
+export const findProduct = async (id) => {
   try {
-    const product =  await DaoProductos.findOne({_id: id})
+    const product = await DaoProductos.findOne({ _id: id });
     return product;
   } catch (error) {
-    console.log('Error al obtener productos: ', error); 
+    console.log("Error al obtener productos: ", error);
   }
-}
+};
 
-export const putProduct = async  (updateProduct, id) => {
+export const putProduct = async (updateProduct, id) => {
   try {
-    const updateStatus =  await DaoProductos.updateOne({_id: id}, {$set: {title: updateProduct.title, price: updateProduct.price, thumbnail: updateProduct.thumbnail}})
+    const updateStatus = await DaoProductos.updateOne(
+      { _id: id },
+      {
+        $set: {
+          title: updateProduct.title,
+          price: updateProduct.price,
+          thumbnail: updateProduct.thumbnail,
+        },
+      }
+    );
     if (updateStatus?.ok === 1) {
-      const product = await DaoProductos.findOne({_id: id});
+      const product = await DaoProductos.findOne({ _id: id });
       return product;
     }
     return null;
   } catch (error) {
-    console.log('Error al obtener productos: ', error); 
+    console.log("Error al obtener productos: ", error);
   }
-}
+};
 
 export const removeProduct = async (id) => {
   try {
-    const removeStatus =  await DaoProductos.findOneAndRemove({_id: id})
+    const removeStatus = await DaoProductos.findOneAndRemove({ _id: id });
     return removeStatus;
   } catch (error) {
-    console.log('Error al obtener productos: ', error); 
+    console.log("Error al obtener productos: ", error);
   }
-}
+};
 
 export const insertProduct = async (newProduct) => {
   try {
-    return await DaoProductos.create({title: newProduct.title, price: newProduct.price, thumbnail: newProduct.thumbnail});
+    return await DaoProductos.create({
+      title: newProduct.title,
+      price: newProduct.price,
+      thumbnail: newProduct.thumbnail,
+    });
   } catch (error) {
-    console.log('Error al insertar mensaje: ', error); 
+    console.log("Error al insertar mensaje: ", error);
   }
-}
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 const socket = io.connect();
 
 socket.on('productos', productos => {
@@ -42,16 +43,33 @@ const addProduct = () => {
 }
 
 socket.on('messages', data => {
-  if (data.length) {
-    render(data);
+  console.log('Normalized data: ', data)
+  const normalizedDataLength = JSON.stringify(data).length
+  console.log(`normalizedDataLength`, normalizedDataLength)
+  
+  const schemaAuthor = new normalizr.schema.Entity('author', {}, { idAttribute: 'email'})
+  const messages = new normalizr.schema.Entity('messages', {
+    author: schemaAuthor,
+  }, {idAttribute: '_id'})
+
+  if (data) {
+    const denormalizedData = normalizr.denormalize(data.result, [messages], data.entities)
+    console.log('Denormalized data: ', denormalizedData)
+    const denormalizedDataLength = JSON.stringify(denormalizedData).length
+    console.log(`denormalizedDataLength`, denormalizedDataLength)
+    const compressionRate = (normalizedDataLength * 100) / denormalizedDataLength
+    console.log(`compressionRate`, Math.round(compressionRate))
+    const html2 = `<h3>Porcentaje de compresión: ${Math.round(compressionRate)}%</h3>`
+    document.getElementById('compressionDiv').innerHTML = html2;
+    render(denormalizedData);
   }
 });
 
 function render(data) {
     const html = data.map((elem, index) => {
       return(`<div>
-            <span class="text-primary"><strong>${elem.author}</strong></span> <span style="color: brown;">${elem.date}</span>:
-            <em>${elem.text}</em>  </div>`)
+            <span class="text-primary"><strong>${elem.author.email}</strong></span> <span style="color: brown;">${elem.date}</span>:
+            <em>${elem.text}</em> <em><img src=${elem.text} alt="user avatar" /></em> </div>`)
     }).join(" ");
     document.getElementById('messages').innerHTML = html;
 }
@@ -60,9 +78,14 @@ function addMessage(e) {
     const time = new Date();
 
     const mensaje = {
-      author: document.getElementById('username').value,
+      email: document.getElementById('userEmail').value,
+      nombre: document.getElementById('username').value,
+      apellido: document.getElementById('userLastName').value,
+      edad: document.getElementById('userAge').value,
+      alias: document.getElementById('alias').value,
+      avatar: document.getElementById('avatar').value,
       text: document.getElementById('texto').value,
-      date: `(${time.getDay()}/${time.getMonth()}/${time.getFullYear()}) ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+      date: `(${time.getDate()}/${time.getMonth()}/${time.getFullYear()}) ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
     };
     socket.emit('new-message', mensaje);
 
