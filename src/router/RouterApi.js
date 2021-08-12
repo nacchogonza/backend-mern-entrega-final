2e3eea9c96035e1ae7a336c25da4afb058cc5765
nacchogonza <nachomgonzalez93@gmail.com>
2021-08-12 00:17:47 -0300

    improve PUT tests

--- src/router/RouterApi.js
+++ src/router/RouterApi.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-import { 
-  getProductController, 
-  getProductsController, 
-  insertProductController, 
-  putProductController, 
-  removeProductController
+import {
+  getProductController,
+  getProductsController,
+  insertProductController,
+  putProductController,
+  removeProductController,
 } from "../controller/controllers.js";
 
 const routerApi = express.Router();
@@ -15,9 +15,10 @@ routerApi.use(express.urlencoded({ extended: true }));
 routerApi.get("/productos", async (req, res) => {
   const data = await getProductsController();
   if (!data.length) {
-    res.json({ error: "no hay productos cargados" });
+    res.status(404).json({ error: "no hay productos cargados" });
+    return
   }
-  res.json(data);
+  res.status(200).json(data);
 });
 
 routerApi.post("/productos", async (req, res) => {
@@ -28,17 +29,21 @@ routerApi.post("/productos", async (req, res) => {
     price: data.price,
     thumbnail: data.thumbnail,
   });
-  res.json(newProduct);
+  res.status(200).json(newProduct);
 });
 
 routerApi.get("/productos/:id", async (req, res) => {
   const filterProduct = await getProductController(req.params.id);
-  if (!filterProduct) res.json({ error: "producto no encontrado" });
-  res.json(filterProduct);
+  if (!filterProduct) {
+    res.status(404).json({ error: "producto no encontrado" });
+    return
+  }
+  res.status(200).json(filterProduct);
 });
 
 routerApi.put("/productos/:id", async (req, res) => {
   const data = req.body;
+  console.log(data)
   data.price = parseFloat(data.price);
   const updateProduct = await putProductController(
     {
@@ -48,14 +53,20 @@ routerApi.put("/productos/:id", async (req, res) => {
     },
     req.params.id
   );
-  if (!updateProduct) res.json({ error: "producto no encontrado" });
-  res.json(updateProduct);
+  if (!updateProduct) {
+    res.status(404).json({ error: "producto no encontrado" });
+    return;
+  }
+  res.status(200).json(updateProduct);
 });
 
 routerApi.delete("/productos/:id", async (req, res) => {
   const deleteProduct = await removeProductController(req.params.id);
-  if (!deleteProduct) res.json({ error: "producto no encontrado" });
-  res.json(deleteProduct);
+  if (!deleteProduct) {
+    res.status(404).json({ error: "producto no encontrado" });
+    return;
+  }
+  res.status(200).json(deleteProduct);
 });
 
 export { routerApi };
--- test/api.test.js
+++ test/api.test.js
@@ -1,24 +1,57 @@
-import supertest from 'supertest';
-import chai from 'chai'
-import { getProduct } from './generador/productos.js'
+import supertest from "supertest";
+import chai from "chai";
+import { getProduct } from "./generador/productos.js";
 
-const request = supertest('http://localhost:8080')
-const expect = chai.expect
+const request = supertest("http://localhost:8080");
+const expect = chai.expect;
 
-describe('test api rest', () => {
-  describe('GET', () => {
-    it('return 404 code when route not exist', async () => {
-      let response = await request.get('/api/test-fail')
+const PRODUCT_ID_GET = "60c13ee89fc20bce240eefd3";
+
+describe("test api rest", () => {
+  describe("GET", () => {
+    it("return 404 code when route not exist", async () => {
+      let response = await request.get("/api/test-fail");
       // console.log(response);
-      expect(response.status).to.eql(404)
-    })
+      expect(response.status).to.eql(404);
+    });
 
-    it('return 200 code when call /productos', async () => {
-      const product = getProduct()
-      console.log(product)
-      let response = await request.get('/api/productos')
+    it("return 200 code when call /productos", async () => {
+      let response = await request.get("/api/productos");
 
-      expect(response.status).to.eql(200)
-    })
-  })
-})
+      expect(response.status).to.eql(200);
+    });
+
+    it("return 404 code when call /productos/:id and id not exists", async () => {
+      let response = await request.get("/api/productos/1");
+
+      expect(response.status).to.eql(404);
+    });
+
+    it("return 200 code when call /productos/:id and id exists", async () => {
+      const product = getProduct();
+      console.log(product);
+      let response = await request.get(`/api/productos/${PRODUCT_ID_GET}`);
+
+      expect(response.status).to.eql(200);
+    });
+  });
+
+  describe("PUT", () => {
+    it("return 404 code when id not exist", async () => {
+      let response = await request.put("/api/productos/1");
+      // console.log(response);
+      expect(response.status).to.eql(404);
+    });
+
+    it("return 200 code when call /productos", async () => {
+      const producto = {
+        title: 'test',
+        price: 234.56,
+        thumbnail: 'AAA'
+      }
+      let response = await request.put(`/api/productos/${PRODUCT_ID_GET}`).send(producto);
+      // console.log(response);
+      expect(response.status).to.eql(200);
+    });
+  });
+});
