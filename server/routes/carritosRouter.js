404c2b9317f72bb37c15166c493c9f05de3cabb2
nacchogonza <nachomgonzalez93@gmail.com>
2021-10-05 21:10:48 -0300

    finish cart view (con eliminacion de productos del carrito)

--- server/routes/carritosRouter.js
+++ server/routes/carritosRouter.js
@@ -2,9 +2,6 @@ import express from 'express';
 import CarritosController from '../controller/carritosController.js';
 const router = express.Router();
 
-import UsuariosController from '../controller/usuariosController.js';
-import { verifyJWT } from '../utils/functions.js';
-
 class RouterCarritos {
   constructor() {
     this.carritosController = new CarritosController(); // inicializar el controller
